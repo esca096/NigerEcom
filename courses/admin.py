fb69545292bcc771e3b7df5b5f47766f380a4c00
esca096 <toshhibana@gmail.com>
2024-09-02 10:43:45 +0100

    eighth commit

--- courses/admin.py
+++ courses/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
-from courses.models import Courses
+from courses.models import Courses, Language, Framework, School, Student, Profile
 # Register your models here.
 
 admin.site.register(Courses)
+
+#one to many 
+admin.site.register(Language)
+
+class AdminFramework(admin.ModelAdmin):
+    list_display = ('name', 'language')
+admin.site.register(Framework, AdminFramework)
+
+
+#many to many
+admin.site.register(School)
+admin.site.register(Student)
+
+#one to one
+admin.site.register(Profile)
--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -16,4 +17,45 @@ class Courses(models.Model):
     
     def get_absolute_url(self):
         return reverse("courses:course-detail", kwargs={"pk": self.pk})
+    
+    
+
+#One to Many  Relationship 
+
+class Language(models.Model):
+    name = models.CharField(max_length=20)
+    
+    
+    def __str__(self):
+        return self.name
+    
+class Framework(models.Model):
+    name = models.CharField(max_length=20)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    
+    
+    def __str__(self):
+        return self.name
+    
+# many to many  relationship 
+
+class School(models.Model):
+    name = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.name
+    
+class Student(models.Model):
+    name = models.CharField(max_length=30)
+    school = models.ManyToManyField(School)
+    
+    def __str__(self):
+        return self.name
+    
+
+# one to one 
+
+class Profile(models.Model):
+    school = models.CharField(max_length=200)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     
